ac74116546205be5c8cbcea289d8d31dc9a5e68d
AMIRTHAMOZHIVS <amirthashan06@gmail.com>
2025-09-03 12:11:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (91.63%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.c
+++ 2556-convert-the-temperature/convert-the-temperature.c
@@ -2,7 +2,7 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 double* convertTemperature(double celsius, int* returnSize) {
-     static double returnSiz[2];
+    static double returnSiz[2];
     returnSiz[0] = celsius+273.15;
     returnSiz[1] = (celsius*1.8)+32.00;
      *returnSize = 2;
