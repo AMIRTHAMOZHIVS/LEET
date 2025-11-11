477c2e8833bbaace1e084f2abfe53c133a228884
AMIRTHAMOZHIVS <amirthashan06@gmail.com>
2025-11-11 22:27:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (10.73%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.c
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    int leftdepth = maxDepth(root->left);
+    int rightdepth = maxDepth(root->right);
+
+    int max;
+    if (leftdepth > rightdepth) {
+        max = leftdepth;
+    } else if (rightdepth > leftdepth) {
+        max = rightdepth;
+    }
+
+    return max + 1;
+}
