c31dc1f8515b3ec10995c5838ccd49451ad1da72
AMIRTHAMOZHI V S <amirthashan06@gmail.com>
2026-02-14 19:52:01 +0530

    Time: 5 ms (34.28%) | Memory: 47.1 MB (14.96%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,29 +1,23 @@
 class Solution {
-    public int romanToInt(String s) {        
-        int i=0,n=s.length();
-        int ans=0;
-        while(i<n){
-            char x=s.charAt(i);
-            if(i==n-1)ans+=getVal(x);
-            else if(getVal(x)>=getVal(s.charAt(i+1)))ans+=getVal(x);
-            else{
-                ans+=getVal(s.charAt(i+1))-getVal(x);
-                i++;
+    public int romanToInt(String s) {
+        int res = 0;
+        Map<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
+                res -= roman.get(s.charAt(i));
+            } else {
+                res += roman.get(s.charAt(i));
             }
-            i++;
         }
-        return ans;
-    }
-    public int getVal(char x) {
-        switch (x) {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
-            default: return 0;
-        }
+
+        return res + roman.get(s.charAt(s.length() - 1));        
     }
 }
