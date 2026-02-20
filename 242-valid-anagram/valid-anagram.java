53536b141300e494d1764a19f872bfd50bfaa1ea
AMIRTHAMOZHI V S <amirthashan06@gmail.com>
2026-02-20 11:41:06 +0530

    Time: 3 ms (91.16%) | Memory: 44.3 MB (83.07%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] alp = new int[26];
+        for(int i=0;i<s.length();i++) alp[s.charAt(i)-'a']++;
+         for(int i=0;i<t.length();i++) alp[t.charAt(i)-'a']--;
+         for(int i: alp ) if(i!=0) return false;
+         return true;
+
+
+    }
+}
