c43e3e837daa379a452af7441df009400cd58880
AMIRTHAMOZHI V S <amirthashan06@gmail.com>
2026-06-11 11:15:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (16.54%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,19 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
+        if (n <= 2) return n;
+
         int[] dp = new int[n + 1];
-        return fib(n, dp);
-    }
 
-    public int fib(int n, int[] dp) {
-        if (n <= 3) {
-            return n;
+        dp[1] = 1;
+        dp[2] = 2;
+
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
-        if (dp[n] != 0) {
-            return dp[n];
-        }
-
-        dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
         return dp[n];
     }
 }
