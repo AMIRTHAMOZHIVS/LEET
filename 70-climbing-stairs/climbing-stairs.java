fe9a7a3f105172c493ef25ebf073eb8c497e3fa2
AMIRTHAMOZHI V S <amirthashan06@gmail.com>
2026-06-11 10:52:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (34.58%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        int n1=1,n2=2,sum=0;
+    if(n<=3){
+        return n;
+    }
+    for(int i=3;i<=n;i++){
+        sum=n1+n2;
+        n1=n2;
+        n2=sum;    }
+    return sum;
+    }
+}
